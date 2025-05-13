8d317c3abfa7b720df7cf7a4f80fbe37834608b5
Bibiane Caron <you@example.com>
2025-05-13 00:46:44 -0400

    Ajout des classes du générateur

---
+++ Gererateur_Scenario/Gererateur_Scenario/Scenario.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gererateur_Scenario
+{
+    class Scenario
+    {
+        private List<Aeroport> aeroports;
+        private List<Evenement> evenements;
+
+        public List<Aeroport> Aeroports
+        {
+            get { return aeroports; }
+            set { aeroports = value; }
+        }
+
+        public List<Evenement> Evenements
+        {
+            get { return evenements; }
+            set { evenements = value; }
+        }
+        
+        public Scenario()
+        {
+            aeroports = new List<Aeroport>();
+            evenements = new List<Evenement>();
+        }
+
+        public void AjouterAeroport(Aeroport aeroport)
+        {
+            aeroports.Add(aeroport);
+        }
+        public void SupprimerAeroport(Aeroport aeroport)
+        {
+            aeroports.Remove(aeroport);
+        }
+        public void ModifierAeroport(Aeroport aeroport)
+        {
+        }
+        public void AjouterEvenement(Evenement evenement)
+        {
+            evenements.Add(evenement);
+        }
+        public void SupprimerEvenement(Evenement evenement)
+        {
+            evenements.Remove(evenement);
+        }
+        public void ModifierEvenement(Evenement evenement)
+        {
+        }
+        public List<Aeroport> GetAeroports()
+        {
+            return aeroports;
+        }
+        public List<Evenement> GetEvenements()
+        {
+            return evenements;
+        }
+        public void ImporterScenario(string cheminFichier)
+        {
+        }
+        public void ExporterScenario(string cheminFichier)
+        {
+        }
+
+
+
+    }
+}
